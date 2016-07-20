d6210e2e7e3f539e93573661239cb616fb0c032a
Derek Lin <Derek Lin>
2016-07-19 23:41:03 -0700

    check in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { TIMER_DIRECTIVES } from './timer/timer';
+import { TASKS_DIRECTIVES } from './tasks/tasks';
+import { SHARED_PROVIDERS } from './shared/shared';
 
 @Component({
   moduleId: module.id,
-  selector: 'app-root',
+  selector: 'pomodoro-app',
+  directives: [TIMER_DIRECTIVES, TASKS_DIRECTIVES],
+  providers: [SHARED_PROVIDERS],
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
-}
+
+export class AppComponent {}
